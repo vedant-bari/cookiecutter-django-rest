6ba63d41c13c3efce5f6c71773f37fe805c90f4b
vedant2222 <vedantbari40@gmail.com>
2019-10-20 21:51:19 +0530

    updated cors settings

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
@@ -26,7 +26,7 @@ class Common(Configuration):
         'rest_framework',            # utilities for rest apis
         'rest_framework.authtoken',  # token authentication
         'django_filters',            # for filtering rest endpoints
-
+        'corsheaders',    
         # Your apps
         '{{cookiecutter.app_name}}.users',
 
@@ -36,6 +36,7 @@ class Common(Configuration):
     MIDDLEWARE = (
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
+        'corsheaders.middleware.CorsMiddleware',
         'django.middleware.common.CommonMiddleware',
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
